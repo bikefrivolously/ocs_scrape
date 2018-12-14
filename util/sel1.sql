e70621624719fa818d8c3a97b43deb13a87d2d5a
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2018-12-14 11:15:12 -0500

    Updated to reflect to schema

--- util/sel1.sql
+++ util/sel1.sql
@@ -1,9 +1,7 @@
-SELECT p.vendor, p.strain, v.weight, vs.price, vs.time
+SELECT p.vendor, p.strain, v.weight, vs.price, vs.available, vs.time
 FROM product AS p
-JOIN product_variant as pv
-ON p.id = pv.product_id
 JOIN variant as v
-ON v.id = pv.variant_id
+ON v.product_id = p.id
 JOIN variant_stock AS vs
 ON vs.id = v.id
 ;
